e79598c5954c5db9e330420359a04d150e569deb
mabdellahi <100428238+mabdellahi@users.noreply.github.com>
2022-12-08 11:00:19 -0800

    Add files via upload

--- dashboardm.py
+++ dashboardm.py
@@ -31,25 +31,11 @@ def main():
             # Données après préprocessing
         df = pd.read_csv('df_test_output.csv')
 
-            # Données test avant features engineering
-            # data_test = pd.read_csv(PATH + 'application_test.csv')
-        #z = ZipFile("application_test.zip")
-        #data_test = pd.read_csv(z.open('application_test.csv'), index_col='SK_ID_CURR', encoding='utf-8')
-        data_test = pd.read_csv('application_test.csv')
-
-            # Données train avant features engineering
-        #z = ZipFile("application_test.zip")
-        #data_train = pd.read_csv(z.open('application_train.csv'), index_col='SK_ID_CURR', encoding='utf-8')
-        data_train = pd.read_csv('application_train.csv')
-
             # Description des features
-        #z = ZipFile("HomeCredit_columns_description.zip")
-        #description = pd.read_csv(z.open('HomeCredit_columns_description.csv'), usecols=['Row', 'Description'],\
-         #                         index_col=0, encoding='unicode_escape')
         description = pd.read_csv('HomeCredit_columns_description.csv',usecols=['Row', 'Description'],\
-                                  index_col=0, encoding='unicode_escape')
+                                 index_col=0, encoding='unicode_escape')
 
-        return df, data_test, data_train, description
+        return df,  description
 
  # -----------------------------------------------------------------------#
    # @st.cache
@@ -64,21 +50,119 @@ def main():
     def get_client_info(data, id_client):
         client_info = data[data['SK_ID_CURR']==int(id_client)]
         return client_info
-# -----------------------------------------------------------#
+# --------------------------------------------------------------------------------------#
+
+    def plot_distribution(applicationDF, feature, client_feature_val, title):
+
+        if (not (math.isnan(client_feature_val))):
+            fig = plt.figure(figsize=(10, 4))
+
+            t0 = applicationDF.loc[applicationDF['TARGET'] == 0]
+            t1 = applicationDF.loc[applicationDF['TARGET'] == 1]
+
+            if (feature == "DAYS_BIRTH"):
+                sns.kdeplot((t0[feature] / -365).dropna(), label='Remboursé', color='g')
+                sns.kdeplot((t1[feature] / -365).dropna(), label='Défaillant', color='r')
+                plt.axvline(float(client_feature_val / -365), color="blue", linestyle='--', label='Position Client')
+
+            elif (feature == "DAYS_EMPLOYED"):
+                sns.kdeplot((t0[feature] / 365).dropna(), label='Remboursé', color='g')
+                sns.kdeplot((t1[feature] / 365).dropna(), label='Défaillant', color='r')
+                plt.axvline(float(client_feature_val / 365), color="blue", linestyle='--', label='Position Client')
+
+            else:
+                sns.kdeplot(t0[feature].dropna(), label='Remboursé', color='g')
+                sns.kdeplot(t1[feature].dropna(), label='Défaillant', color='r')
+                plt.axvline(float(client_feature_val), color="blue", linestyle='--', label='Position Client')
+
+            plt.title(title, fontsize='20', fontweight='bold')
+            plt.legend()
+            plt.show()
+            st.pyplot(fig)
+        else:
+            st.write("Comparaison impossible car la valeur de cette variable n'est pas renseignée (NaN)")
+
+        # -----------------------------------------------------------------
+        # @st.cache
+    def univariate_categorical(applicationDF, feature, client_feature_val, titre, ylog=False, label_rotation=False,\
+                                   horizontal_layout=True):
+        if (client_feature_val.iloc[0] != np.nan):
+            temp = applicationDF[feature].value_counts()
+            df1 = pd.DataFrame({feature: temp.index, 'Number of contracts': temp.values})
+            categories = applicationDF[feature].unique()
+            categories = list(categories)
+
+                # Calculate the percentage of target=1 per category value
+            cat_perc = applicationDF[[feature, 'TARGET']].groupby([feature], as_index=False).mean()
+            cat_perc["TARGET"] = cat_perc["TARGET"] * 100
+            cat_perc.sort_values(by='TARGET', ascending=False, inplace=True)
+
+        if (horizontal_layout):
+                fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 5))
+        else:
+                fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(20, 24))
+
+                    # 1. Subplot 1: Count plot of categorical column
+                    # sns.set_palette("Set2")
+                s = sns.countplot(ax=ax1,
+                                      x=feature,
+                                      data=applicationDF,
+                                      hue="TARGET",
+                                      order=cat_perc[feature],
+                                      palette=['g', 'r'])
+
+                pos1 = cat_perc[feature].tolist().index(client_feature_val.iloc[0])
+                    # st.write(client_feature_val.iloc[0])
+
+                    # Define common styling
+                ax1.set(ylabel="Nombre de clients")
+                ax1.set_title(titre, fontdict={'fontsize': 15, 'fontweight': 'bold'})
+                ax1.axvline(int(pos1), color="blue", linestyle='--', label='Position Client')
+                ax1.legend(['Position Client', 'Remboursé', 'Défaillant'])
+
+                    # If the plot is not readable, use the log scale.
+        if ylog:
+                ax1.set_yscale('log')
+                ax1.set_ylabel("Count (log)", fontdict={'fontsize': 15, 'fontweight': 'bold'})
+        if (label_rotation):
+                s.set_xticklabels(s.get_xticklabels(), rotation=90)
+
+                    # 2. Subplot 2: Percentage of defaulters within the categorical column
+                s = sns.barplot(ax=ax2,
+                                    x=feature,
+                                    y='TARGET',
+                                    order=cat_perc[feature],
+                                    data=cat_perc,
+                                    palette='Set2')
+
+                pos2 = cat_perc[feature].tolist().index(client_feature_val.iloc[0])
+                    # st.write(pos2)
+
+        if (label_rotation):
+                s.set_xticklabels(s.get_xticklabels(), rotation=90)
+                plt.ylabel('Pourcentage de défaillants [%]', fontsize=10)
+                plt.tick_params(axis='both', which='major', labelsize=10)
+                ax2.set_title(titre + " (% Défaillants)", \
+                                  fontdict={'fontsize': 15, 'fontweight': 'bold'})
+                ax2.axvline(int(pos2), color="blue", linestyle='--', label='Position Client')
+                ax2.legend()
+                plt.show()
+                st.pyplot(fig)
+        else:
+                st.write("Comparaison impossible car la valeur de cette variable n'est pas renseignée (NaN)")
+
 
         #####------------------------------------------------------------------------------#####
 
             #----------Chargement des données--------------#
 
-    df, data_test, data_train, description = load_data()
+    df, description = load_data()
 
     features_dropped = ['Unnamed: 0', 'SK_ID_CURR', 'TARGET']
     relevant_features = [col for col in df if col not in features_dropped]
 
     print("df shape", df.shape)
-    print("data_test shape", data_test.shape)
-    print("data_train shape", data_train.shape)
-    print("description shape", description.shape)
+    #print("description shape", description.shape)
     print("df columns", df.columns)
 
 
@@ -101,7 +185,7 @@ def main():
         st.write("## Actions à réaliser")
         credit_decision = st.checkbox("Afficher la décision de crédit")
         client_details = st.checkbox("Afficher les informations du client")
-        #client_comparison = st.checkbox("Comparer aux autres clients")
+        client_comparison = st.checkbox("Comparer aux autres clients")
         shap_general = st.checkbox("Afficher la feature importance globale")
         if (st.checkbox("Aide description des features")):
             list_features = description.index.to_list()
@@ -138,7 +222,7 @@ def main():
     st.write("ID Client Sélectionné :", id_client)
 
     if (int(id_client) in id_list):
-        client_info = get_client_info(data_test, id_client)
+        client_info = get_client_info(df, id_client)
 
     # -------------------------------------------------------
     # Affichage de la décision de crédit
@@ -149,9 +233,10 @@ def main():
     #------------------------------------ Appel de l'API--------------------------------- :
     #API_url = "http://127.0.0.1:5000/credit/" + str(id_client)
         with st.spinner('Chargement du score du client...'):
+            seuil=55
             ID=str(id_client)
-            #response = json.loads(requests.get("http://127.0.0.1:5000/predict/{}".format(ID)).content)
-            response = json.loads(requests.get("https://dashboard.heroku.com/apps/mabdellahi-api-p7/predict/{}".format(ID)).content)
+            response = json.loads(requests.get("http://127.0.0.1:5000/predict/{}".format(ID)).content)
+            #response = json.loads(requests.get("https://dashboard.heroku.com/apps/mabdellahi-api-p7/predict/{}".format(ID)).content)
             print(response)
             API_data=response
             #--------------------------------------
@@ -225,26 +310,28 @@ def main():
     personal_info_cols = {
         'CODE_GENDER': "GENRE",
         'DAYS_BIRTH': "AGE",
-        'NAME_FAMILY_STATUS': "STATUT FAMILIAL",
+      #  "NAME_FAMILY_STATUS_MARRIED":"STATUT FAMILIAL",
+       # 'NAME_FAMILY_STATUS': "STATUT FAMILIAL",
         'CNT_CHILDREN': "NB ENFANTS",
         'FLAG_OWN_CAR': "POSSESSION VEHICULE",
         'FLAG_OWN_REALTY': "POSSESSION BIEN IMMOBILIER",
-        'NAME_EDUCATION_TYPE': "NIVEAU EDUCATION",
-        'OCCUPATION_TYPE': "EMPLOI",
+        #'NAME_EDUCATION_TYPE': "NIVEAU EDUCATION",
+        #'OCCUPATION_TYPE': "EMPLOI",
         'DAYS_EMPLOYED': "NB ANNEES EMPLOI",
         'AMT_INCOME_TOTAL': "REVENUS",
         'AMT_CREDIT': "MONTANT CREDIT",
-        'NAME_CONTRACT_TYPE': "TYPE DE CONTRAT",
+       # 'NAME_CONTRACT_TYPE': "TYPE DE CONTRAT",
         'AMT_ANNUITY': "MONTANT ANNUITES",
-        'NAME_INCOME_TYPE': "TYPE REVENUS",
+        #'NAME_INCOME_TYPE': "TYPE REVENUS",
         'EXT_SOURCE_1': "EXT_SOURCE_1",
         'EXT_SOURCE_2': "EXT_SOURCE_2",
         'EXT_SOURCE_3': "EXT_SOURCE_3",
 
     }
 
-    default_list = \
-        ["GENRE", "AGE", "STATUT FAMILIAL", "NB ENFANTS", "REVENUS", "MONTANT CREDIT"]
+    default_list = ["GENRE", "AGE", "NB ENFANTS", "REVENUS", "MONTANT CREDIT"]
+         #["GENRE", "AGE", "STATUT FAMILIAL", "NB ENFANTS", "REVENUS", "MONTANT CREDIT"]
+
     numerical_features = ['DAYS_BIRTH', 'CNT_CHILDREN', 'DAYS_EMPLOYED', 'AMT_INCOME_TOTAL', 'AMT_CREDIT',\
                           'AMT_ANNUITY', 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']
 
@@ -255,13 +342,13 @@ def main():
         st.header('Informations du client')
 
         with st.spinner('Chargement des informations du client...'):
+             #client_info = get_client_info(df, id_client)
              personal_info_df = client_info[list(personal_info_cols.keys())]
              personal_info_df['SK_ID_CURR'] = client_info['SK_ID_CURR']
              personal_info_df.rename(columns=personal_info_cols, inplace=True)
              personal_info_df["AGE"] = round(personal_info_df["AGE"] / 365 * (-1)).astype(int)
              personal_info_df["NB ANNEES EMPLOI"] = round(personal_info_df["NB ANNEES EMPLOI"] / 365 * (-1)).astype(int)
-             filtered = st.multiselect("Choisir les informations à afficher", \
-                                      options=list(personal_info_df.columns), \
+             filtered = st.multiselect("Choisir les informations à afficher", options=list(personal_info_df.columns), \
                                       default=list(default_list))
              df_info = personal_info_df[filtered]
              df_info['SK_ID_CURR'] = client_info['SK_ID_CURR']
@@ -276,27 +363,34 @@ def main():
     #### ---------------Comparaison du client choisi à d'autres clients--------------- #### -
     # -------------------------------------------------------
 
-   # if (client_comparison):
-   #     st.header(' Comparaison aux autres clients')
-        # st.subheader("Comparaison avec l'ensemble des clients")
-    #    with st.expander("Explication de la comparaison faite"):
-    #        st.write(
-    #            "Quand on sélectionne une variable, un graphique montrant la distribution de cette variable selon la classe (remboursé ou défaillant) est affiché avec une matérialisation du positionnement du client actuel.")
+        if (client_comparison):
+            st.header(' Comparaison aux autres clients')
+                # st.subheader("Comparaison avec l'ensemble des clients")
+        with st.expander("Explication de la comparaison faite"):
 
-    #    with st.spinner('Chargement de la comparaison liée à la variable sélectionnée'):
-    #        var = st.selectbox("Sélectionner une variable", list(personal_info_cols.values()))
-    #        feature = list(personal_info_cols.keys()) \
-    #            [list(personal_info_cols.values()).index(var)]
+            st.write(
+                        "Quand on sélectionne une variable, un graphique montrant la distribution de cette variable selon la classe (remboursé ou défaillant) est affiché avec une matérialisation du positionnement du client actuel.")
 
+        with st.spinner('Chargement de la comparaison liée à la variable sélectionnée'):
+            var = st.selectbox("Sélectionner une variable", list(personal_info_cols.values()))
+            feature = list(personal_info_cols.keys()) \
+                        [list(personal_info_cols.values()).index(var)]
 
-            #----------------------------------------------------------------------------
-    #        if(st.checkbox("Afficher les clients similaires")):
+            if (feature in numerical_features):
+                        plot_distribution(df, feature, client_info[feature], var)
+            elif (feature in rotate_label):
+                        univariate_categorical(df, feature, client_info[feature], var, False, True)
+            elif (feature in horizontal_layout):
+                        univariate_categorical(df, feature, client_info[feature], var, False, True, True)
+            else:
+                        univariate_categorical(df, feature, client_info[feature], var)
 
-                nbrs = NearestNeighbors(n_neighbors=10, algorithm='ball_tree').fit(df)
+                #----------------------------------------------------------------------------
+            if(st.checkbox("Afficher les clients similaires")):
+               nbrs = NearestNeighbors(n_neighbors=10, algorithm='ball_tree').fit(df)
              #On récupère l'indice des plus proches voisins du client
-
-                indices = nbrs.kneighbors(df[0:1])[1].flatten()
-                st.dataframe(data_test.iloc[indices])
+               indices = nbrs.kneighbors(df[0:1])[1].flatten()
+               st.dataframe(df.iloc[indices])
 
 
     # -------------------------------------------------------
